5b1e684e23dc0f4d8cd9bbac08b3a25530172044
HermanoIsoton <hermanoisoton023@gmail.com>
2026-07-15 00:02:47 -0300

    inicio

---
+++ js2.js
@@ -0,0 +1,16 @@
+// ex1
+const p1 = document.getElementById('p1');
+const b1 = document.getElementById('b1');
+
+b1.addEventListener('click', () => {
+ p1.textContent = 'Gladston';
+});
+
+
+// ex2
+const p2 = document.getElementById('p2');
+const i1 = document.getElementById('i1');
+
+i1.addEventListener('input', () => {
+
+});
