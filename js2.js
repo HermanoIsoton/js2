51fdebce38e5cdc1c335a067239fcf9e47107913
HermanoIstoton <hermanoisoton023@gmail.com>
2026-07-15 17:06:20 -0300

    salvar

--- js2.js
+++ js2.js
@@ -1,16 +1,76 @@
 // ex1
-const p1 = document.getElementById('p1');
-const b1 = document.getElementById('b1');
+const p1 = document.getElementById("p1");
+const b1 = document.getElementById("b1");
 
-b1.addEventListener('click', () => {
- p1.textContent = 'Gladston';
+b1.addEventListener("click", () => {
+  p1.textContent = "Gladston";
 });
 
-
 // ex2
-const p2 = document.getElementById('p2');
-const i1 = document.getElementById('i1');
+const p2 = document.getElementById("p2");
+const i1 = document.getElementById("i1");
 
-i1.addEventListener('input', () => {
+i1.addEventListener("input", function () {
+  p2.textContent = i1.value;
+});
 
+// ex3
+const m1 = document.getElementById("m1");
+const b2 = document.getElementById("b2");
+
+b2.addEventListener("dblclick", () => {
+  m1.src =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIx5S4nr5evdioCiVZbWqCqVYeqnxQh2z4au9j1C8dEQ&s=10";
+});
+
+// ex4
+const b3 = document.getElementById("b3");
+
+const dica = document.getElementById("dica");
+b3.addEventListener("mouseover", () => {
+  dica.textContent = "dica do botao";
+});
+b3.addEventListener("mouseout", () => {
+  dica.textContent = "";
+});
+
+// ex5
+const numLetras = document.getElementById("numeroLetras");
+const campo = document.getElementById("texto");
+campo.addEventListener("input", () => {
+  const texto = campo.value;
+  numLetras.textContent = texto.length;
+});
+
+// ex6
+const txt = document.getElementById("txt");
+const send = document.getElementById("enviar");
+send.addEventListener("click", function () {
+  if (txt.value != "") {
+    txt.value = "enviado!";
+  } else {
+    txt.value = "Campo Obrigatorio!";
+  }
+});
+
+// ex7
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    alert("voce apertou esc!");
+  }
+});
+
+// ex 8
+const largura = document.getElementById("largura");
+const altura = document.getElementById("altura");
+window.addEventListener("resize", function () {
+  largura.textContent = window.innerHeight;
+  altura.textContent = window.innerWidth;
+});
+
+// ex9  
+let alturaTotal = document.body.innerHeight;
+window.addEventListener("scroll", function(){
+   alturaTotal = alturaTotal + window.scrollY;
+   console.log(alturaTotal);
 });
